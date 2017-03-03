5dd4144cc672cf48904b5238023867bcd5b6130d
Bobby Kissinger <kissinger645@gmail.com>
2017-03-03 13:54:11 -0500

    Added state and city pages

--- CL/Controllers/HomeController.cs
+++ CL/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,34 @@ namespace CL.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+
+            ViewBag.States = db.Locations.ToList();
+                return View();
         }
 
         [Route("u/{UserName}")]
         public ActionResult UserPage()
         {
+            return View();
+        }
+
+        [Route("s/{State}")]
+        public ActionResult State(string State)
+        {
+            ViewBag.Cities = db.Locations.Where(l => l.StateName == State);
+            return View();
+        }
+
+        [Route("c/{City}")]
+        public ActionResult City(string City)
+        {
+            ViewBag.SubSale = db.Categories.Where(c => c.Main == "Sale");
+            ViewBag.SubHousing = db.Categories.Where(c => c.Main == "Housing");
+            ViewBag.SubHousing = db.Categories.Where(c => c.Main == "Services");
             return View();
         }
     }
